3de1d6dcb3093ad5039c2cff4953e61c3d984006
kalicrafty <74075397+kalicrafty@users.noreply.github.com>
2021-03-31 19:13:19 +0200

    PimpMyChair-V2
    The v2 of this... strange artclass project ? with comments, optimizations, tweaks and new content...

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
 PimpMyChair
-
-Just an artclass project than add part chair image together...
-
-Made in c++ require Qt6 or more.
--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <QApplication>
-#include "Window.h"
+#include "BuildWindow.h"
+
 
 int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
-    app.setApplicationName("Pimp my chair !");
-    app.setWindowIcon(QIcon(app.applicationDirPath()+"/Assets/icon.png"));
-    Window window;
-    window.show();
-    return app.exec();
+    QApplication app(argc, argv); //creating a Qt application
+    app.setApplicationName("Pimp my chair !"); //set the window name
+    app.setWindowIcon(QIcon(app.applicationDirPath()+"/Assets/icon.png")); // set the window icon
+    BuildWindow window; //creating a new "window" object
+    window.show(); //show the window
+    return app.exec(); //execute the application
 }
